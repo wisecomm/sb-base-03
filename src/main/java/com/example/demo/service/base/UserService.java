c3ae84034bb95a56b0fbb7600e3da851c6be908f
danyoh <danyoh#gmail.com>
2025-03-27 23:10:28 +0900

    패키지 이름 정리

--- src/main/java/com/example/demo/service/base/UserService.java
+++ src/main/java/com/example/demo/service/base/UserService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.service.base;
 
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.map.UserMap;
@@ -6,7 +6,6 @@ import com.example.demo.model.map.UserMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
